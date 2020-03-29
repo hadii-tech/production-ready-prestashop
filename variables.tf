19fe1b432876a1c5de52dabe5b6e21a41c570f23
zir0-93 <muntazirfadhel@gmail.com>
2020-03-29 18:32:54 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_account_id" {}
-variable "db_password" {}
-variable "aws_region" {
-  description = "AWS region e.g. us-east-1 (Please specify a region supported by the Fargate launch type)"
-}
+variable "aws_region" {}
 variable "env" {
   description = "Name of environment being deployed into"
+}
+variable "db_password" {
+  description = "Password to use for the Prestashop database."
 }
